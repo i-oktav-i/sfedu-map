a57bd52248f3e44cc4e936732d5d82627ce80679
a.taranenko <i_okatv_i@mail.ru>
2024-12-22 14:06:06 +0300

    feat: Добавлен заголовок

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,17 +1,29 @@
 import { ThemeSelect } from '@features/ThemeSelect';
-import { Box, Flex } from '@radix-ui/themes';
+import { Box, Grid, Text } from '@radix-ui/themes';
 import { FC } from 'react';
 
 export const Header: FC = () => {
   return (
-    <Flex asChild justify={'between'} align={'center'} py={'3'} px={'6'}>
+    <Grid
+      asChild
+      columns={'20% 1fr 20%'}
+      rows={'auto'}
+      justify={'between'}
+      align={'center'}
+      py={'3'}
+      px={'6'}
+    >
       <header>
         <Box asChild width={'40px'} height={'40px'} style={{ borderRadius: '50%' }}>
           <img src={'/favicon.svg'} alt={'logo'} />
         </Box>
 
+        <Text asChild align={'center'}>
+          <h1>Карта кампусов</h1>
+        </Text>
+
         <ThemeSelect />
       </header>
-    </Flex>
+    </Grid>
   );
 };
