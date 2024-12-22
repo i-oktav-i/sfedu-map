0cb9b5b10dac7ff313eff880693923536fabdad3
a.taranenko <i_okatv_i@mail.ru>
2024-12-22 12:26:27 +0300

    feat: Добавлен Radix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </StrictMode>,
 );
