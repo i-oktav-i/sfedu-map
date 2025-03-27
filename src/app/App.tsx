7a1106010ca390e33205b475e7791d8e9d5181ba
a.taranenko <i_okatv_i@mail.ru>
2025-03-28 00:08:45 +0300

    feat: Замена recoil на zustand

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { RouterProvider } from 'react-router';
-import { RecoilRoot } from 'recoil';
 
 import { router } from './router';
 
@@ -11,9 +10,7 @@ import { SomethingWrongWidget } from '@widgets/SomethingWrongWidget';
 import './index.css';
 
 export const App: FC = () => (
-  <RecoilRoot>
-    <ErrorBoundary FallbackComponent={SomethingWrongWidget}>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
-  </RecoilRoot>
+  <ErrorBoundary FallbackComponent={SomethingWrongWidget}>
+    <RouterProvider router={router} />
+  </ErrorBoundary>
 );
