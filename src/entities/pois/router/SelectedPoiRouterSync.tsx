9a9977e0df7e697fdee48aab81195db655379331
a.taranenko <i_okatv_i@mail.ru>
2025-05-13 22:38:05 +0300

    fix: Исправлена синхронизация id и url

--- src/entities/pois/router/SelectedPoiRouterSync.tsx
+++ src/entities/pois/router/SelectedPoiRouterSync.tsx
@@ -1,32 +1,42 @@
 import { useLocaleType } from '@shared/locale';
 import { ChildrenProp, PoiId } from '@shared/types';
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { generatePath, useNavigate, useParams } from 'react-router';
 import { getPoisList } from '../getPoisList';
 import { useSelectedPoiStore } from '../store';
 
 export const SelectedPoiRouterSync: FC<ChildrenProp> = ({ children }) => {
   const navigate = useNavigate();
-  const { id } = useParams<{ id?: PoiId }>();
+  const { id } = useParams<{ id?: `${PoiId}` }>();
 
   const { selectedPoi, setSelectedPoi } = useSelectedPoiStore();
   const { locale } = useLocaleType();
 
+  const [inited, setInited] = useState(false);
+
   const selectedPoiId = selectedPoi?.id ?? null;
 
   useEffect(() => {
-    if (id === selectedPoiId) return;
+    if (!inited || id === selectedPoiId) return;
 
-    navigate(generatePath('/:id?', { id: selectedPoi?.id ?? null }));
-  }, [selectedPoi]);
+    navigate(generatePath('/:id?', { id: selectedPoiId }));
+  }, [selectedPoiId]);
 
   useEffect(() => {
-    if (id === selectedPoiId) return;
+    if (selectedPoiId && id === selectedPoiId) return;
 
     const selectedPoiData = getPoisList(locale).find((poi) => poi.id === id);
 
     setSelectedPoi(selectedPoiData ?? null);
   }, [id]);
 
-  return <>{children}</>;
+  useEffect(() => setInited(true), []);
+
+  return (
+    <>
+      {selectedPoi?.address ? <title>{selectedPoi.address}</title> : null}
+
+      {children}
+    </>
+  );
 };
